b8ff9d9fd8405e11bf1e4bdcd3462de6dc1ed9ea
Rumman Sheikh <rummansheikh2004@gmail.com>
2026-06-06 23:01:45 +0600

    Add files via upload

---
+++ storefront/app/components/utils/FacebookPixel.jsx
@@ -0,0 +1,59 @@
+"use client";
+
+import Script from "next/script";
+import { useEffect } from "react";
+import { usePathname, useSearchParams } from "next/navigation";
+
+const FACEBOOK_PIXEL_ID = "4119555701606031";
+
+export default function FacebookPixel() {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    if (typeof window !== "undefined" && window.fbq) {
+      window.fbq("track", "PageView");
+    }
+  }, [pathname, searchParams]);
+
+  return (
+    <>
+      <Script
+        id="facebook-pixel"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;
+            n.push=n;
+            n.loaded=!0;
+            n.version='2.0';
+            n.queue=[];
+            t=b.createElement(e);
+            t.async=!0;
+            t.src=v;
+            s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}
+            (window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+
+            fbq('init', '4119555701606031');
+            fbq('track', 'PageView');
+          `,
+        }}
+      />
+
+      <noscript>
+        <img
+          height="1"
+          width="1"
+          style={{ display: "none" }}
+          alt=""
+          src={`https://www.facebook.com/tr?id=4119555701606031&ev=PageView&noscript=1`}
+        />
+      </noscript>
+    </>
+  );
+}
