f7c2c55aa5abbc4a77ce5e132abd490ec7a4eb32
Rumman Sheikh <rummansheikh2004@gmail.com>
2026-06-11 23:37:21 +0600

    Integrate Google Tag Manager in layout.js
    Added Google Tag Manager script and noscript iframe for tracking.

--- storefront/app/layout.js
+++ storefront/app/layout.js
@@ -7,6 +7,7 @@ import BottomNavigation from "./components/layout/BottomNavigation";
 import AuthInitializer from "./components/utils/AuthInitializer";
 import { Toaster } from "react-hot-toast";
 import FacebookPixel from "./components/utils/FacebookPixel";
+import Script from "next/script";
 
 const banglaRegular = localFont({
   variable: "--font-bangla-regular",
@@ -45,11 +46,41 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <meta name="apple-mobile-web-app-title" content="swapnoborno" />
+     <Script
+          id="gtm"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){
+                w[l]=w[l]||[];
+                w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});
+                var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),
+                dl=l!='dataLayer'?'&l='+l:'';
+                j.async=true;
+                j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+                f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-5LWBCXGK');
+            `,
+          }}
+        />
       </head>
       <body
         // ensure the body is at least viewport height so descendant height-full works
         className={`${geistSans.variable} ${geistMono.variable} ${banglaRegular.variable} antialiased bg-main text-zinc-900 min-h-screen w-full`}
       >
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5LWBCXGK"
+            height="0"
+            width="0"
+            style={{
+              display: "none",
+              visibility: "hidden",
+            }}
+          />
+        </noscript>
         <FacebookPixel />
         <AuthInitializer>
           <Navbar />
